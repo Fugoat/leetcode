5957df09274d9d044af09c64b68d6d40edc829ec
Fugoat <92004743+Fugoat@users.noreply.github.com>
2021-10-13 12:16:49 +0800

    二叉树连接

---
+++ leetcode 10.11-二叉树连接.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+//class Solution {
+//public:
+//    Node* connect(Node* root) {
+//        if (root->left == NULL)
+//        {
+//            return root;
+//        }
+//
+//        root->left->next = root->right;
+//        if (root->next != NULL)
+//        {
+//            root->right->next = root->next->left;
+//        }
+//
+//
+//        connect(root->left);
+//        connect(root->right);
+//    }
+//};
+
+class solution{
+public:
+    Node * connect(Node * root) {
+         if (root== NULL)
+         {
+             return root;
+         }
+
+
+
+        if (root->left != NULL)
+        {
+            root->left->next = root->right;
+            if (root->next != NULL)
+        {
+            root->right->next = root->next->left;
+        }
+           connect(root->left);
+           connect(root->right);
+        }
+       return root;
+
+
+    }
+};
+
+int main()
+{
+    solution s1;
+}
+
+
