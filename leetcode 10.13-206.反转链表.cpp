e9bca05cc3c0cafddbe3b232fe15e9ea6d6bfe62
Fugoat <92004743+Fugoat@users.noreply.github.com>
2021-10-14 16:49:34 +0800

    反转链表

---
+++ leetcode 10.13-206.反转链表.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+ 
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+//class Solution {
+  //迭代法
+//public:
+//    ListNode* reverseList(ListNode* head) {
+//        ListNode* pre = NULL;
+//        ListNode* current = head;
+//        ListNode* nexttemp;
+//        while (current != NULL)
+//        {
+//            nexttemp = current->next;
+//            current->next = pre;
+//            pre = current;
+//            current = nexttemp;
+//
+//        }
+//        return pre;
+//    }
+//};
+int main()
+{
+   
+}
+
+
